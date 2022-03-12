28d511828af812844afd71744d63280f39ce13c7
Sajib Ahmed <101242621+SajibAhmed01@users.noreply.github.com>
2022-03-12 12:40:07 +0600

    Add files via upload

---
+++ fibonakki number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int fibo,first=0,second=1,n,count=0;
+
+    printf("Please enter the number:");
+
+    scanf("%d",&n);
+
+    while(fibo<n)
+    {
+        if(count<=1)
+        {
+            fibo=count;
+        }
+
+    else
+    {
+        fibo=first+second;
+        first=second;
+        second=fibo;
+    }
+     printf("fibo =%d\n",fibo);
+     count++;
+    }
+    return 0;
+}
