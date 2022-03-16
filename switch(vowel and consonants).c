fd14d075e5d9c8a9eccc172d3ad3947647230dd6
Sajib Ahmed <101242621+SajibAhmed01@users.noreply.github.com>
2022-03-16 12:51:21 +0600

    Add files via upload

---
+++ switch(vowel and consonants).c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int main ()
+{
+    char ch;
+
+    printf("Please enter the any letter:");
+
+    scanf("%c",&ch);
+
+    switch(ch)
+    {
+    case 'a':
+        printf("vowel\n");
+        break;
+
+    case 'e':
+        printf("vowel\n");
+        break;
+
+
+    case 'i':
+        printf("vowel\n");
+        break;
+
+    case 'o':
+        printf("vowel\n");
+        break;
+
+    case 'u':
+        printf("vowel\n");
+        break;
+
+    case' A':
+        printf("vowel\n");
+        break;
+
+
+    case 'E':
+        printf("vowel\n");
+        break;
+
+
+    case 'I':
+        printf("vowel\n");
+        break;
+
+    case' O':
+        printf("vowel\n");
+        break;
+
+    case 'U':
+        printf("vowel\n");
+        break;
+
+    default:
+
+        printf("consonant\n");
+        break;
+    }
+   return 0;
+}
