145be8c687b5929bb719c8b94b8d08164e006129
Sajib Ahmed <101242621+SajibAhmed01@users.noreply.github.com>
2022-04-10 01:52:42 +0600

    Add files via upload

---
+++ array 2D/array matrix subtraction.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,x,y,b[10][10];
+    printf("How many two array number:");
+    scanf("%d %d",&x,&y);
+    int  a[x][y];
+
+    printf("Input the matrix:\n");
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            printf("a[%d][%d]=",i,j);
+            scanf("%d",&a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("MARTIX:\n");
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            b[i][j]=a[i][j]-a[i][j];
+        }
+        printf("\n");
+    }
+    printf("MARTIX addition A+A =\n");
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
